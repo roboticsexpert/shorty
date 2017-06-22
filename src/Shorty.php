0a2b00ebc1680820e8ca60debe91c6860ee3060c
Mahdi Youseftabar <roboticsexpert@gmail.com>
2017-06-22 11:54:25 +0430

    ignore check url

--- src/Shorty.php
+++ src/Shorty.php
@@ -22,7 +22,7 @@ class Shorty extends ShortLinkAbstractInterface
      */
     public function __construct()
     {
-        $this->apiKey = 'key='. config('shorty.api_key') .'';
+        $this->apiKey = 'key=' . config('shorty.api_key') . '';
     }
 
     /**
@@ -37,20 +37,20 @@ class Shorty extends ShortLinkAbstractInterface
         if ($this->urlCheck($longUrl)) {
             $request = array
             (
-                'http'=> array
+                'http' => array
                 (
-                    'method'        =>  "POST",
-                    'header'        =>  "Accept-language: en\r\n".
-                                        "Content-Type: application/json\r\n",
+                    'method' => "POST",
+                    'header' => "Accept-language: en\r\n" .
+                        "Content-Type: application/json\r\n",
                     'ignore_errors' => true,
-                    'content'       => json_encode(array('longUrl' => $longUrl)),
+                    'content' => json_encode(array('longUrl' => $longUrl)),
                 )
             );
 
-            $params = (!empty($this->apiKey)) ? '?'.$this->apiKey : '';
-            $data = $this->request($this->api.$params, stream_context_create($request));
-            $data = json_decode($data,true);
-            $data = $this->validate($data,'shorten');
+            $params = (!empty($this->apiKey)) ? '?' . $this->apiKey : '';
+            $data = $this->request($this->api . $params, stream_context_create($request));
+            $data = json_decode($data, true);
+            $data = $this->validate($data, 'shorten');
 
             return $data['id'];
         }
@@ -61,17 +61,17 @@ class Shorty extends ShortLinkAbstractInterface
     /**
      * Expands an URL with the goo.gl/XXXXX format to its original form.
      *
-     * @param  string                                 $shortUrl
+     * @param  string $shortUrl
      * @return mixed
      * @throws Exceptions\InvalidApiResponseException
      */
     public function expand($shortUrl)
     {
         if ($this->urlCheck($shortUrl)) {
-            $params = (!empty($this->apiKey)) ? '?'.implode('&',array($this->apiKey,"shortUrl={$shortUrl}")) : "?shortUrl={$shortUrl}";
-            $data = $this->request($this->api.$params,stream_context_create( array('http' => array('ignore_errors' => true) ) ) );
-            $data = json_decode($data,true);
-            $data = $this->validate($data,'expand');
+            $params = (!empty($this->apiKey)) ? '?' . implode('&', array($this->apiKey, "shortUrl={$shortUrl}")) : "?shortUrl={$shortUrl}";
+            $data = $this->request($this->api . $params, stream_context_create(array('http' => array('ignore_errors' => true))));
+            $data = json_decode($data, true);
+            $data = $this->validate($data, 'expand');
 
             return $data['longUrl'];
         }
@@ -81,17 +81,17 @@ class Shorty extends ShortLinkAbstractInterface
     /**
      * Returns all data available, including statistics from a shortened URL (goo.gl/XXXXX)
      *
-     * @param  string                                 $shortUrl
+     * @param  string $shortUrl
      * @return array
      * @throws Exceptions\InvalidApiResponseException
      */
     public function stats($shortUrl)
     {
         if ($this->urlCheck($shortUrl)) {
-            $params = (!empty($this->apiKey)) ? '?'.implode('&',array($this->apiKey,"shortUrl={$shortUrl}","projection=FULL")) : implode('&',array("shortUrl={$shortUrl}","projection=FULL"));
-            $data = $this->request($this->api.$params,stream_context_create( array('http' => array('ignore_errors' => true) ) ));
-            $data = json_decode($data,true);
-            $data = $this->validate($data,'expand');
+            $params = (!empty($this->apiKey)) ? '?' . implode('&', array($this->apiKey, "shortUrl={$shortUrl}", "projection=FULL")) : implode('&', array("shortUrl={$shortUrl}", "projection=FULL"));
+            $data = $this->request($this->api . $params, stream_context_create(array('http' => array('ignore_errors' => true))));
+            $data = json_decode($data, true);
+            $data = $this->validate($data, 'expand');
 
             return $data;
         }
@@ -99,31 +99,30 @@ class Shorty extends ShortLinkAbstractInterface
     }
 
 
-
     /**
      * Validates Google URL Shortener response and throws error messages, if any.
      *
-     * @param  array                                  $data
+     * @param  array $data
      * @param $method
      * @return array
      * @throws Exceptions\InvalidApiResponseException
      */
     protected function validate(&$data, $method)
     {
-        if ( is_array($data) === false ) {
+        if (is_array($data) === false) {
             throw new InvalidApiResponseException('Google Url Shortener returned a response that could not be handled');
         }
 
         if (!empty($data['error']['errors'])) {
             $error = json_encode($data['error']['errors']);
-            throw new InvalidApiResponseException('Google Url Shortener returned the following error: '.$error);
+            throw new InvalidApiResponseException('Google Url Shortener returned the following error: ' . $error);
         }
 
         if (empty($data['status']) && $method == 'expand') {
             throw new InvalidApiResponseException('Google Url Shortener has no status');
         }
 
-        if ( !empty($data['status']) && $data['status']!='OK' && $method == 'expand') {
+        if (!empty($data['status']) && $data['status'] != 'OK' && $method == 'expand') {
             throw new InvalidApiResponseException('Google Url Shortener returned an invalid status');
         }
 
@@ -137,6 +136,7 @@ class Shorty extends ShortLinkAbstractInterface
 
         return $data;
     }
+
     /**
      * Validates if the URL to submit actually exists and is accessible.
      *
@@ -146,6 +146,7 @@ class Shorty extends ShortLinkAbstractInterface
      */
     protected function urlCheck($url)
     {
+        return true;
         // first check if url has http:// prefix, if not, add it
         $parsed = parse_url($url);
 
@@ -158,7 +159,7 @@ class Shorty extends ShortLinkAbstractInterface
         if (!strpos($file_headers[0], "404 Not Found") > 0) {
             return true;
         }
-        
+
         return false;
     }
 }
